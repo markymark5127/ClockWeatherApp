3094fcbe3794ee054f95a6bcd7c9434fe5a3934d
Mark Mayne <markmayne@ymail.com>
2025-06-18 08:39:33 -0400

    Fix digit display and adjust card rounding

---
+++ ClockWeatherApp/RoundedCorners.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+import UIKit
+
+struct RoundedCorners: Shape {
+    var radius: CGFloat
+    var corners: UIRectCorner
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect,
+                                byRoundingCorners: corners,
+                                cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
