de6bae13832d437e22e3e04f222cb794b9b2071d
Mark Mayne <markmayne@ymail.com>
2025-06-18 08:24:29 -0400

    Improve widget layout and digit halves

--- ClockWeatherApp/SingleDigitView.swift
+++ ClockWeatherApp/SingleDigitView.swift
@@ -16,17 +16,8 @@ struct SingleDigitView: View {
 
     var body: some View {
         let width = CGFloat(height * max(1, text.count))
-        let fontSize: CGFloat = height * 1.5
-        let font = UIFont(name: fontName, size: fontSize) ?? .systemFont(ofSize: fontSize)
-        let sample = ("8" as NSString).size(withAttributes: [.font: font])
-        let offset = (height - sample.height) / 2
-        Text(text)
-            .font(.custom(fontName, size: fontSize))
-            .offset(y: offset)
-            .foregroundColor(.white)
-            .frame(width: width, height: height, alignment: type.alignment)
-            .padding(type.padding, -height / 5)
-            .clipped()
+        DigitHalfView(digit: text, fontName: fontName, clipTop: type == .top)
+            .frame(width: width, height: height)
             .background(Color.black)
             .cornerRadius(height / 5)
             .padding(type.padding, -height / 10)
